e4c514f82f480053ab26d165a70edc7456d4afb2
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-10-12 11:18:24 +0530

    initial commit

---
+++ Assets/Plugins/Mars Motion Toolkit/Dependencies/Requisites/Always.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Always", menuName = "Mars Motion/Dependency Requisites/Always")]
+public class Always : DependencyRequisite
+{
+	// methods //
+
+
+	// method: determine the (boolean) state of this Dependency Requisite //
+	public override bool state()
+	{
+		return true;
+	}
+}
