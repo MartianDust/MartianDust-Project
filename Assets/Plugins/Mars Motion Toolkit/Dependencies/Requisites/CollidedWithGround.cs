f7fa098b4acfefbe4416fb9d359bd633407fe7ad
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-10-13 21:17:37 +0530

    initial commit

---
+++ Assets/Plugins/Mars Motion Toolkit/Dependencies/Requisites/CollidedWithGround.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Collided With Ground", menuName = "Mars Motion/Dependency Requisites/Collided With Ground")]
+public class CcollidedWithGround : DependencyRequisite
+{
+	// methods //
+
+	
+	// method: determine the (boolean) state of this Dependency Requisite //
+	public override bool state()
+	{
+		return TerrainResponse.collidedWithGround();
+	}
+}
