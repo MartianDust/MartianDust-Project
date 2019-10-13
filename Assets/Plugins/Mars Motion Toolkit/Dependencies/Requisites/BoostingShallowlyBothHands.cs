66c4e7daa7532187d5452b2abad1f9b98457583d
tusharchoudhary0003 <tusharchoudhary0003@gmail.com>
2019-10-13 21:14:37 +0530

    initial commit

---
+++ Assets/Plugins/Mars Motion Toolkit/Dependencies/Requisites/BoostingShallowlyBothHands.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Boosting Shallowly Both Hands", menuName = "Mars Motion/Dependency Requisites/Boosting Shallowly Both Hands")]
+public class BoostingShallowlyBothHands : DependencyRequisite
+{
+	// methods //
+
+	
+	// method: determine the (boolean) state of this Dependency Requisite //
+	public override bool state()
+	{
+		return Booster.bothHandsAreBoostingShallowly();
+	}
+}
